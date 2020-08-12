e3de93d6d3fee87b36d0e9d0d94c10274faef7ea
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-08-12 21:57:03 +0300

    Lift up ARRAY JOIN step.

--- src/Interpreters/ExpressionActions.cpp
+++ src/Interpreters/ExpressionActions.cpp
@@ -1100,22 +1100,22 @@ ExpressionActionsPtr ExpressionActions::splitActionsBeforeArrayJoin(const NameSe
     {
         if (action.type == ExpressionAction::PROJECT)
         {
-            NamesWithAliases split_projection;
+            NamesWithAliases split_aliases;
             NamesWithAliases depend_projection;
             for (auto & pair : action.projection)
             {
-                if (array_join_dependent_columns.count(pair.first))
+                if (pair.second.empty() || array_join_dependent_columns.count(pair.first))
                 {
                     if (!pair.second.empty())
                         array_join_dependent_columns.insert(pair.second);
                     depend_projection.emplace_back(std::move(pair));
                 }
                 else
-                    split_projection.emplace_back(std::move(pair));
+                    split_aliases.emplace_back(std::move(pair));
             }
 
-            if (!split_projection.empty())
-                split_actions->add(ExpressionAction::project(split_projection));
+            if (!split_aliases.empty())
+                split_actions->add(ExpressionAction::addAliases(split_aliases));
 
             if (!depend_projection.empty())
                 new_actions.emplace_back(ExpressionAction::project(depend_projection));
